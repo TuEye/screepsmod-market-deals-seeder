2df481ae16f27ceb584ba90ddd81a208e48ff0c5
TuEye <26883979+TuEye@users.noreply.github.com>
2026-09-03 22:52:30 +0200

    Add seeder configuration example

---
+++ marketDealsSeederConfig.example.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  mode: 'maintain',
+  days: 14,
+  minDealsPerDay: 10,
+  amountPerDeal: 1000,
+  intervalHours: 12,
+  countAllDeals: true,
+  blacklist: []
+};
